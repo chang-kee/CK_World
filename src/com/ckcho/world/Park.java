4f019a86b9a0d3714ac3cdfab45f9cbe01337772
CCK <whckdrl52@naver.com>
2021-05-21 17:29:52 +0900

    CK_World opening commit

---
+++ src/com/ckcho/world/Park.java
@@ -0,0 +1,5 @@
+package com.ckcho.world;
+
+public enum Park {
+	수목원, 동물원, 식물원, 공원
+}
---
+++ src/com/ckcho/world/World.java
@@ -0,0 +1,14 @@
+package com.ckcho.world;
+
+import com.sun.tools.jdeps.Analyzer.Type;
+
+public class World {
+
+	public static void main(String[] args) {
+		Park world = Park.동물원;
+		System.out.println("CK월드에 오신걸 환영합니다");
+		System.out.println(Park.공원);
+
+	}
+
+}
