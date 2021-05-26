ceea5b7f43e501a37da2c93bb3422c1f854f7777
CCK <whckdrl52@naver.com>
2021-05-26 18:03:41 +0900

    다방 요일별 스페셜 메뉴 추가

--- src/com/ckcho/sports/World.java
+++ src/com/ckcho/sports/World.java
@@ -1,11 +1,15 @@
 package com.ckcho.sports;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class World {
 
 	public static void main(String[] args) {
-		Sports world = Sports.이피엘;
+		Sports aworld = Sports.이피엘;
 		System.out.println("CK월드에 오신걸 환영합니다");
 		Sports shirts[] = Sports.values();
 		
@@ -19,6 +23,14 @@ public class World {
 				Sports.분데스리가.toString() );
 	
 		System.out.println(league);
+		int idx = LocalDate.now().getDayOfYear() % shirts.length;
+//		shirts // 배열학습 후 완성
+		String weekDay = LocalDateTime.now().format(
+				DateTimeFormatter.ofPattern("E") //  E =>Weekday
+				.withLocale(Locale.KOREAN));
+		System.out.println(weekDay + "요일 스페셜: " + shirts[idx]);		
+//		Sports.이피엘.ordinal()
+	
 		System.out.println("원하는 유니폼 종류를 입력: ");
 		
 		Scanner scanner = new Scanner(System.in);
