e2ad50ff7c3604b2db5ba201dc06f56428756e39
CCK <whckdrl52@naver.com>
2021-05-27 15:54:19 +0900

    use Switch

--- src/com/ckcho/sports/Sports.java
+++ src/com/ckcho/sports/Sports.java
@@ -1,5 +1,5 @@
 package com.ckcho.sports;
 
 public enum Sports {
-	이피엘, 라리가, 세리에, 분데스리가
+	라리가, 세리에, 분데스리가
 }
--- src/com/ckcho/sports/World.java
+++ src/com/ckcho/sports/World.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 public class World {
 
 	public static void main(String[] args) {
-		Sports aworld = Sports.이피엘;
+		Sports aworld = Sports.라리가;
 		System.out.println("CK월드에 오신걸 환영합니다");
 		Sports shirts[] = Sports.values();
 		
@@ -18,24 +18,39 @@ public class World {
 		 *  나중에 Enum List 사용하여 깔끔하게 정리
 		 */
 		String league = String.join("/",
-				Sports.라리가.toString(),
-				Sports.세리에.toString(),
-				Sports.분데스리가.toString() );
+				Sports.라리가.toString() + (Sports.라리가.ordinal() + 1),
+				Sports.세리에.toString() + (Sports.세리에.ordinal() + 1),
+				Sports.분데스리가.toString() +(Sports.분데스리가.ordinal() + 1) );
 	
 		System.out.println(league);
 		int idx = LocalDate.now().getDayOfYear() % shirts.length;
-//		shirts // 배열학습 후 완성
+
+		
 		String weekDay = LocalDateTime.now().format(
 				DateTimeFormatter.ofPattern("E") //  E =>Weekday
 				.withLocale(Locale.KOREAN));
 		System.out.println(weekDay + "요일 스페셜: " + shirts[idx]);		
-//		Sports.이피엘.ordinal()
-	
+
 		System.out.println("원하는 유니폼 종류를 입력: ");
 		
 		Scanner scanner = new Scanner(System.in);
 		int 제품번호 = scanner.nextInt();
-		System.out.println("제품번호" + 제품번호 + "를 선택하셨습니다");
+
+		
+		Sports worlds = Sports.values()[제품번호 -1]; 
+		switch (worlds) {
+		case 라리가:
+			System.out.println("라리가를 선택하셨습니다.");
+			break;
+		case 세리에:
+			System.out.println("세리에를 선택하셨습니다.");
+			break;
+		case 분데스리가:
+			System.out.println("분데스리가를 선택하셨습니다.");
+			// break;
+		default:
+			break;
+		}
 		scanner.close();
 		
 		return ;
